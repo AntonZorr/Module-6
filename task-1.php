bc73a8a9daf4b6e1cbb52c63c2f08ba8cb9e214b
AntonZorr <118671625+AntonZorr@users.noreply.github.com>
2022-11-28 15:51:44 +0300

    Add files via upload

---
+++ task-1.php
@@ -0,0 +1,45 @@
+<?php
+
+$textStorage = [];
+
+function addText($title = 0, $text = 0, &$textStorage)
+{
+    $textStorage[] = array("title" => $title, "text" => $text);
+}
+
+addText('1', 'One', $textStorage);
+addText('3', 'Three', $textStorage);
+
+
+//print_r($textStorage);
+
+function removeText($index, &$textStorage)
+{
+    if (array_key_exists($index, $textStorage)) {
+        unset($textStorage[$index]);
+        return true;
+        //echo 'Значение с ключом ' . $index . ' удалено' . PHP_EOL;
+    } else {
+        return false;
+        //echo 'Значение с ключом ' . $index . ' не найдено.' . PHP_EOL;
+    }
+}
+
+//var_dump(removeText(0, $textStorage));
+//var_dump(removeText(5, $textStorage));
+//print_r($textStorage);
+
+function editText(int $index, string $newTitle = '', string $newText = '', &$textStorage)
+{
+    if (array_key_exists($index, $textStorage)) {
+        $textStorage[$index] = array("title" => $newTitle, "text" => $newText);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+var_dump(editText(1, '7', 'Seven',$textStorage));
+var_dump(editText(2, '7', 'Seven',$textStorage));
+var_dump(editText(10, '7', 'Seven',$textStorage));
+print_r($textStorage);
